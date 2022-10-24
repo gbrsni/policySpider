c83e2befded33f5c31c5a8d116cf54b04e81fba9
gbrsni <72036069+gbrsni@users.noreply.github.com>
2022-10-24 18:05:44 +0200

    Moved text goodness check to dedicated function

--- policyspider/spiders/policyspider.py
+++ policyspider/spiders/policyspider.py
@@ -39,6 +39,25 @@ def selenium_get_policy_from_url(url):
 
 	return res
 
+def policy_text_is_good(policy_text):
+	"""Returns true if the input text is considered good.
+	Returns false and what the problems is if a problem is found"""
+	if output_text == "" or output_text is None:
+		print("Couldn't find policy at " + policy_url)
+		return False
+	elif output_text.startswith("404 Not Found") \
+		or output_text.startswith("403 Forbidden") \
+		or output_text.startswith("Forbidden"):
+		print("Bad policy at " + policy_url)
+		return False
+
+	policy_word_count = len(output_text.split())
+	if policy_word_count < 500:
+		print("Policy too short " + policy_url)
+		return False
+
+	return True
+
 def save_policy_text(policy_url, file_name):
 	"""Saves the policy found at the given URL to a text file with name file_name inside DATADIR"""
 	file_name = os.path.join(DATADIR, file_name)
@@ -61,27 +80,7 @@ def save_policy_text(policy_url, file_name):
 		for paragraph in paragraphs:
 			output_text += paragraph.text + " "
 
-	try:
-		if output_text == "" or output_text is None:
-			raise BadPolicyError("Couldn't find policy at " + policy_url)
-		elif output_text.startswith("404 Not Found") \
-			or output_text.startswith("403 Forbidden") \
-			or output_text.startswith("Forbidden"):
-			raise BadPolicyError("Bad policy at " + policy_url)
-	
-		if output_text.startswith("%PDF-"):
-			pdf_file = tempfile.NamedTemporaryFile(suffix = ".pdf", prefix = "policy_")
-			pdf_file.write(response.content)
-			output_text = get_text_from_pdf(pdf_file)
-			pdf_file.close()
-			if output_text == "" or output_text is None:
-				raise BadPolicyError("Couldn't find policy at " + policy_url)
-
-		policy_word_count = len(output_text.split())
-		if policy_word_count < 500:
-			raise BadPolicyError("Policy too short " + policy_url)
-
-	except BadPolicyError:
+	if not policy_text_is_good(output_text):
 		output_text = selenium_get_policy_from_url(policy_url)
 
 	try:
